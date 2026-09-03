bf4af726b1c6621b03e1d3c50cf8fa4d3d85c721
Navneet Kushwaha <naveeraj62@gmail.com>
2026-09-03 23:37:35 +0530

    sum of list using function and loop

---
+++ basics/sum_of_num.py
@@ -0,0 +1,19 @@
+# ye 5 alag parameters ka sum h 
+
+# def sum_of_num(a,b,c,d,e):
+#     return a+b+c+d+e
+# x=sum_of_num(10,20,60,50,30)
+# print(x)
+
+
+# sum of list 
+
+def sum_list(numbers):
+    total=0
+
+    for num in numbers:
+        total += num
+
+    return total
+x = sum_list([10,50,30,60])    
+print(x)
