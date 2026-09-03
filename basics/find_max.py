86194bcca41b87994f7ff072b788f9f56fb01478
Navneet Kushwaha <naveeraj62@gmail.com>
2026-09-03 23:21:06 +0530

    greatest of 3 numbers

---
+++ basics/find_max.py
@@ -0,0 +1,13 @@
+def maximum(num1,num2,num3):
+    
+    if num1>num2 and num1>num3:
+        return num1 
+
+    elif num2>num3 and num2>num1:
+        return num2 
+    
+    else:
+        return num3 
+
+X = maximum(20,30,15)    
+print(X)
