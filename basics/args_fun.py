a9dc0078ba7390c5cc5461c359392f9f1b709370
Navneet Kushwaha <naveeraj62@gmail.com>
2026-09-05 21:54:46 +0530

    average of no. using *args

---
+++ basics/args_fun.py
@@ -0,0 +1,11 @@
+def average_number(*args):
+    total =0
+    average=0
+    for num in args:
+        total += num
+        average=total/len(args)
+
+    return average
+
+x=average_number(10,20,30,40)
+print(x)
